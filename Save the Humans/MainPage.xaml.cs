f3bc55a10c84fe9e377348a769545bb5c9fa7d69
aforbis <aforbis83091@gmail.com>
2017-09-28 17:13:51 -0500

    Adding functionality of adding enemies to the canvas, and centered the start button.

--- Save the Humans/MainPage.xaml.cs
+++ Save the Humans/MainPage.xaml.cs
@@ -15,6 +15,8 @@ using Windows.UI.Xaml.Navigation;
 using Windows.UI.ViewManagement;
 using Windows.Foundation.Metadata;
 
+using Windows.UI.Xaml.Media.Animation;
+
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Save_the_Humans
@@ -24,6 +26,8 @@ namespace Save_the_Humans
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Random random = new Random();
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -34,6 +38,32 @@ namespace Save_the_Humans
         private void startButton_Click(object sender, RoutedEventArgs e)
         {
             AddEnemy();
+        }
+
+        private void AddEnemy()
+        {
+            ContentControl enemy = new ContentControl();
+
+            enemy.Template = Resources["EnemyTemplate"] as ControlTemplate;
+            AnimateEnemy(enemy, 0, playArea.ActualWidth - 100, "(Canvas.Left)");
+            AnimateEnemy(enemy, random.Next((int)playArea.ActualHeight - 100),
+                random.Next((int)playArea.ActualHeight - 100), "(Canvas.Top)");
+            playArea.Children.Add(enemy);
+        }
+
+        private void AnimateEnemy(ContentControl enemy, double from, double to, string propertyToAnimate)
+        {
+            Storyboard storyboard = new Storyboard() { AutoReverse = true, RepeatBehavior = RepeatBehavior.Forever };
+            DoubleAnimation animation = new DoubleAnimation()
+            {
+                From = from,
+                To = to,
+                Duration = new Duration(TimeSpan.FromSeconds(random.Next(4, 6)))
+            };
+            Storyboard.SetTarget(animation, enemy);
+            Storyboard.SetTargetProperty(animation, propertyToAnimate);
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
         }
     }
 }
